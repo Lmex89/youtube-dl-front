8fab676034de572a46737cb231b60d811016a34d
Lmex89 <bjt.mex@gmail.com>
2023-06-01 17:06:33 -0600

    agregando algo de estilos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import { OverlayTrigger, Popover } from 'react-bootstrap';
 
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [youtubeUrl, setYoutubeUrl] = useState('');
@@ -74,36 +84,43 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1 className="mt-4 mb-4">YouTube Video Downloader</h1>
-      <div className="input-group mb-3">
-        <input type="text" value={youtubeUrl} onChange={handleInputChange} class="form-control" placeholder="Youtube Url" aria-label="Youtube Url" aria-describedby="basic-addon2" />
-        <OverlayTrigger trigger="click" placement="right" overlay={popover} rootClose>
-          <button className="btn btn-primary" type="button" onClick={handleDownload}>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="container">
+        <h1 className="mt-4 mb-4">YouTube Video Downloader</h1>
+        <div className="input-group mb-3">
+          <input type="text" value={youtubeUrl} onChange={handleInputChange} class="form-control" placeholder="Youtube Url" aria-label="Youtube Url" aria-describedby="basic-addon2" />
+          <OverlayTrigger trigger="click" placement="right" overlay={popover} rootClose>
+            {/* <button className="btn btn-primary" type="button" onClick={handleDownload}>
             Download
-          </button>
-        </OverlayTrigger>
-        <button className="btn btn-secondary" type="button" onClick={handleClean}>
+          </button> */}
+            <Button variant="contained" onClick={handleDownload} color="success" >Download</Button>
+          </OverlayTrigger>
+          {/* <button className="btn btn-secondary" type="button" onClick={handleClean}>
           Clean
-        </button>
-      </div>
-      {loading &&
-        <Backdrop
-          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open
-        >
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      }
-      {downloadUrl && url && (
-        <div>
-          <video controls>
-            <source src={url} type="video/mp4" />
-          </video>
-          <a href={url} download>Click here to download the video</a>
+        </button> */}
+          <Button variant="outlined" disableElevation onClick={handleClean} color="secondary" startIcon={<DeleteIcon />} >
+            clean
+          </Button>
         </div>
-      )}
-    </div>
+        {loading &&
+          <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open
+          >
+            <CircularProgress color="inherit" />
+          </Backdrop>
+        }
+        {downloadUrl && url && (
+          <div>
+            <video controls>
+              <source src={url} type="video/mp4" />
+            </video>
+            <a href={url} download>Click here to download the video</a>
+          </div>
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
 
